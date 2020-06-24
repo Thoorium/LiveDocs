f8cdc0af51d4bb8fd66eb483ff9ebd420353482b
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2020-06-24 17:17:19 -0400

    Basic theme.

---
+++ src/LiveDocs.WebApp/Options/LiveDocsOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveDocs.WebApp.Options
+{
+    public class LiveDocsOptions
+    {
+        public string ApplicationName { get; set; } = "LiveDocs";
+    }
+}
