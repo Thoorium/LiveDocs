ea75c5757b04e7c998f9a5f1a2e63a192b0afb0d
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2020-08-06 19:41:31 -0400

    Case insensitive file search for Linux systems.

--- src/LiveDocs.WebApp/Controllers/FileController.cs
+++ src/LiveDocs.WebApp/Controllers/FileController.cs
@@ -3,9 +3,11 @@ using LiveDocs.WebApp.Options;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace LiveDocs.WebApp.Controllers
@@ -15,9 +17,11 @@ namespace LiveDocs.WebApp.Controllers
         private readonly IWebHostEnvironment _HostEnvironment;
         private readonly LiveDocsOptions _Options;
         private readonly IDocumentationService _DocumentationService;
+        private readonly ILogger<FileController> _Logger;
 
-        public FileController(IWebHostEnvironment hostEnvironment, IOptions<LiveDocsOptions> options, IDocumentationService documentationService)
+        public FileController(ILogger<FileController> logger, IWebHostEnvironment hostEnvironment, IOptions<LiveDocsOptions> options, IDocumentationService documentationService)
         {
+            _Logger = logger;
             _HostEnvironment = hostEnvironment;
             _Options = options.Value;
             _DocumentationService = documentationService;
@@ -43,7 +47,12 @@ namespace LiveDocs.WebApp.Controllers
                 path = document.Path;
             else path = Path.Combine(_Options.GetDocumentationFolderAsAbsolute(_HostEnvironment.ContentRootPath).FullName, fullFilename);
 
-            if (!System.IO.File.Exists(path))
+            var found = System.IO.File.Exists(path);
+
+            if (!found && RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                found = TryFindFileCaseInsensitive();
+
+            if (!found)
                 return NotFound($"Not found: {fullFilename}");
 
             var fileStream = new FileStream(path, FileMode.Open);
@@ -62,6 +71,34 @@ namespace LiveDocs.WebApp.Controllers
             if (forceDownload)
                 result.FileDownloadName = fullFilename;
             return result;
+
+            bool TryFindFileCaseInsensitive()
+            {
+                _Logger.LogInformation($"Searching for file {string.Join("/", paths)}.{ext} with insensitive case.");
+
+                var dirInfo = new DirectoryInfo(_Options.GetDocumentationFolderAsAbsolute(_HostEnvironment.ContentRootPath).FullName);
+                var dirEnumOptions = new EnumerationOptions { MatchCasing = MatchCasing.CaseInsensitive };
+
+                // Manually scan the path to the file due to case sensitivity.
+                for (int i = 0; i < paths.Length - 1; i++)
+                {
+                    var subDirInfo = dirInfo.GetDirectories(paths[i], dirEnumOptions);
+                    _Logger.LogDebug($"Found {subDirInfo.Length} matching directory for path {dirInfo.FullName}/{paths[i]}.");
+
+                    if (subDirInfo.Length == 0)
+                        return false;
+
+                    dirInfo = subDirInfo.First();
+                }
+                var fileInfos = dirInfo.GetFiles($"{paths[^1]}.{ext}", dirEnumOptions);
+                _Logger.LogDebug($"Found {fileInfos.Length} matching file for path {dirInfo.FullName}/{paths[^1]}.{ext}.");
+
+                if (fileInfos.Length == 0)
+                    return false;
+
+                path = fileInfos.First().FullName;
+                return true;
+            }
         }
     }
 }
