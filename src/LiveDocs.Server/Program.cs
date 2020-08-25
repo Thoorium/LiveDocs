e370ef57e0da7dd06d90ddfee8327e50ff6bba9e
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2020-08-24 20:43:33 -0400

    Environment variable prefix.

--- src/LiveDocs.Server/Program.cs
+++ src/LiveDocs.Server/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace LiveDocs.Server
 {
@@ -18,6 +13,10 @@ namespace LiveDocs.Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables(prefix: "LiveDocs_");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
