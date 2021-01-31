e1ffffd90e627b0b5b032cfe08c3d1b640397768
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2021-01-30 19:01:43 -0500

    Configurable DrawioSvg default background theme.

--- src/LiveDocs.Shared/Services/Documents/Configuration/DrawioSvgDocumentConfiguration.cs
+++ src/LiveDocs.Shared/Services/Documents/Configuration/DrawioSvgDocumentConfiguration.cs
@@ -4,6 +4,15 @@ namespace LiveDocs.Shared.Services.Documents.Configuration
 {
     public class DrawioSvgDocumentConfiguration : IDocumentConfiguration
     {
+        public enum BackgroundStyle
+        {
+            Light,
+            Dark,
+            Theme
+        }
+
+        public BackgroundStyle? DefaultBackground { get; set; }
+
         [JsonIgnore]
         public bool Enabled { get; set; } = true;
 
